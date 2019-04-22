f376b30d3c83d92c79f2954530d79aa7f3423e7a
benzkji <bnzk@bnzk.ch>
2019-04-22 15:51:56 +0200

    some more files

---
+++ setup.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+from setuptools import setup, find_packages
+import os
+
+
+# not so bad: http://joebergantine.com/blog/2015/jul/17/releasing-package-pypi/
+version = __import__('filebase').__version__
+
+
+def read(fname):
+    # read the contents of a text file
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="django-filebase",
+    version=version,
+    url='http://github.com/benzkji/django-filebase',
+    license='MIT',
+    platforms=['OS Independent'],
+    description="media management for django",
+    long_description=read('PYPI.rst'),
+    author=u'Ben Stähli',
+    author_email='bnzk@bnzk.ch',
+    packages=find_packages(),
+    install_requires=(
+        'Django>=1.11',
+        'easy-thumbnails>=2.0',
+    ),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+)
